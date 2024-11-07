7688779c8ce12d6c0aa392738dcc5702b90e70d0
DmitriiKonopelkin <dm.konopelkin@gmail.com>
2024-11-07 12:02:31 +0300

    add sql-query

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
  
  require 'db.php';
+
+ $result=$conn->query("SHOW TABLES");
+
+ $tables=[];
+
+ while($row=$result->fetch_assoc()) {
+    $tables[]=reset($row);
+ }
+
+ echo "<ul>";
+ foreach($tables as $table) {
+    $colCount=$conn->query("SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='" . $table . "'");
+
+    $colCountRow= $colCount->fetch_row();
+    $cols= $colCountRow[0];
+
+    echo "<li>$table</li>"
+ }
+
+ echo "</ul>";
 ?>

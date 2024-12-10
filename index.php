f939f84cc9a4bc97c4693ccb0ffe6a1d8777162b
DmitriiKonopelkin <dm.konopelkin@gmail.com>
2024-12-10 13:48:53 +0300

    change index.php

--- index.php
+++ index.php
@@ -54,7 +54,10 @@ while ($row = $result->fetch_assoc()) {
 
 echo "<ul>";
 foreach ($tables as $table) {
-    $colCount = $conn->query("SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='" . $table . "'");
+    $colCount = $conn->query("SELECT COUNT(*)
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE  TABLE_SCHEMA = DATABASE() AND TABLE_NAME = '" . $table . "' ");
+
     $colCountRow = $colCount->fetch_row();
     $cols = $colCountRow[0];
     
